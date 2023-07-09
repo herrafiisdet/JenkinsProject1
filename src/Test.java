8ad019e9850e6c6760d5192bc7f6b5ae1103ed82
herrafiisdet <herrafiisdet@gmail.com>
2023-07-09 18:19:09 -0400

    commit 3

--- src/Test.java
+++ src/Test.java
@@ -3,5 +3,6 @@ public class Test {
     public static void main(String[] args) {
 
         System.out.println(" THIS IS MY JENKINS PROJECT N1");
+        System.out.println(" THIS IS MY JENKINS PROJECT N1");
     }
 }
