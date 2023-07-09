16d11471689f334e46cdb03cc0ff9a55b50654c6
herrafiisdet <herrafiisdet@gmail.com>
2023-07-09 12:35:37 -0400

    Initial Commit 1

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         System.out.println("Hello world!");
         for (int i = 0; i < 10; i++){
-            System.out.println("hello world ");
+            System.out.println("hello world version 2 ");
             Thread.sleep(2000);
         }
     }
